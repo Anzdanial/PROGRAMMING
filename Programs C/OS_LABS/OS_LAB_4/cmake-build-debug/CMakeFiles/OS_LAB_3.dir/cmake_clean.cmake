7b72ca95a8b58b80eab922364087c1e5c2f94b43
Anzdanial <anzdanial@gmail.com>
2023-09-23 22:20:11 +0500

    WOW

---
+++ Programs C/OS_LABS/OS_LAB_4/cmake-build-debug/CMakeFiles/OS_LAB_3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_LAB_3.dir/main.c.o"
+  "CMakeFiles/OS_LAB_3.dir/main.c.o.d"
+  "OS_LAB_3"
+  "OS_LAB_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OS_LAB_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Programs C/OS_LABS/OS_LAB_4/cmake-build-debug/CMakeFiles/OS_LAB_4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_LAB_4.dir/main.c.o"
+  "CMakeFiles/OS_LAB_4.dir/main.c.o.d"
+  "OS_LAB_4"
+  "OS_LAB_4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OS_LAB_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
